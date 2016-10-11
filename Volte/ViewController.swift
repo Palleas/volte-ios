57f30e1f1657f67d5bd89d51b0b037e3fcd36cc5
Romain Pouclet <romain.pouclet@gmail.com>
2016-10-11 11:58:36 -0400

    Initial commit

---
+++ Volte/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Volte
+//
+//  Created by Romain Pouclet on 2016-10-11.
+//  Copyright © 2016 Perfectly-Cooked. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
