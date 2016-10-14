089cf12b78408a6e9918b15b50d3546226eb7aed
Romain Pouclet <romain.pouclet@gmail.com>
2016-10-14 13:09:25 -0400

    Store/Retrieve credentials from keychain

--- Volte/AccountController.swift
+++ Volte/AccountController.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import ReactiveSwift
+import RGLockboxIOS
 
 struct Account {
     let username: String
@@ -21,11 +22,25 @@ protocol AccountControllerType {
 }
 
 class AccountController: AccountControllerType {
+    private static let usernameKey = "username"
+    private static let passwordKey = "password"
+
+    private let keychain = RGLockbox(withNamespace: "xyz.voltenetwork") // TODO: inject this, maybe
 
     var account = MutableProperty<Account?>(nil)
 
+    init() {
+        if let username = keychain.stringForKey(AccountController.usernameKey), let password = keychain.stringForKey(AccountController.passwordKey) {
+            print("Auto-auth woooo")
+            self.authenticate(with: username, password: password)
+        }
+    }
+
     func authenticate(with username: String, password: String) {
         account.value = Account(username: username, password: password)
+
+        keychain.setString(username, key: AccountController.usernameKey)
+        keychain.setString(password, key: AccountController.passwordKey)
     }
 
 }
